4f020189d38d1a83f68baf7c8ae3d000a4c2baba
Williams <williamsoditi99@gmail.com>
2022-08-05 11:28:10 +0300

    Feat : Adds homeview and urls

--- NetflixApp/views.py
+++ NetflixApp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+import requests
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
