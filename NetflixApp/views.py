798b6e34d909c2e140704f8f4029d3343e16fbb3
Williams <williamsoditi99@gmail.com>
2022-08-08 09:04:35 +0300

    Feat : Adds urls

--- NetflixApp/views.py
+++ NetflixApp/views.py
@@ -6,3 +6,6 @@ import requests
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'django_registration/register.html')
