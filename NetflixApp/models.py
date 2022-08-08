ee6b2b417694ff2f50bf823c7825c3a6052f15d2
Williams <williamsoditi99@gmail.com>
2022-08-08 09:35:09 +0300

    Feat : Adds models - Create model Movie

--- NetflixApp/models.py
+++ NetflixApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Movie(models.Model):
+    title=models.CharField(max_length=100)
+    genres=models.CharField(max_length=50)
+    poster_path=CloudinaryField('photo')
+    overview=models.CharField(max_length=500)
+    release_date=models.DateTimeField()
+
+
+    def save_movie(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
